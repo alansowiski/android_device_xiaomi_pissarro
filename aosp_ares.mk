50d0fc6872bcadbad358204a1560982e3395bb79
alansowiski <87510678+alansowiski@users.noreply.github.com>
2022-06-22 15:28:40 +0530

    Update aosp_ares.mk

--- aosp_ares.mk
+++ aosp_ares.mk
@@ -21,20 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from ares device makefile
-$(call inherit-product, device/xiaomi/ares/device.mk)
+$(call inherit-product, device/xiaomi/pissarro/device.mk)
 
 # Inherit some source stuff
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ares
-PRODUCT_NAME := aosp_ares
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3 GT
+PRODUCT_DEVICE := pissarro
+PRODUCT_NAME := aosp_pissarro
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 21091116C
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -42,6 +42,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ares \
     PRODUCT_NAME=ares \
-    PRIVATE_BUILD_DESC="ares-user 11 RP1A.200720.011 V12.5.5.0.RKJINXM release-keys"
+    PRIVATE_BUILD_DESC="pissarro-user 11 RP1A.200720.011 V12.5.6.0. RKTINFK release-keys"
 
-BUILD_FINGERPRINT := POCO/aresin/ares:11/RP1A.200720.011/V12.5.5.0.RKJINXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/pissarro/pissarro:11/RP1A.200720.011/V12.5.6.0.RKTINFK:user/release-keys
