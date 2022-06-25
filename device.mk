9412a54396e804e7144c78903261cc930f4a7753
alansowiski <87510678+alansowiski@users.noreply.github.com>
2022-06-25 06:58:40 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -98,24 +98,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
-# Ims
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
-# Init
-PRODUCT_PACKAGES += \
-    init.mt6877.rc \
-    fstab.mt6877 \
-    fstab.mt6877_ramdisk
-
-PRODUCT_COPY_FILES +=  $(DEVICE_PATH)/rootdir/etc/fstab.mt6877:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/etc/fstab.mt6877
-
 # Input
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
@@ -125,10 +107,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
-# Libshims
-PRODUCT_PACKAGES += \
-    libshim_vtservice \
-    libshim_showlogo
 
 # Priv-app Permissions
 PRODUCT_COPY_FILES += \
